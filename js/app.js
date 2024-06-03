8a66fa4b3704b58d981ab4c79b3038d9438485ae
melissaceee <cheng.melissa@outlook.com>
2024-06-02 20:18:43 -0400

    step 1

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+/*-------------------------------- Constants --------------------------------*/
+//5) Define the required constants.
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+//1) Define the required variables used to track the state of the game.
+
+let board
+let turn
+let winner
+let tie
+
+
+/*------------------------ Cached Element References ------------------------*/
+//2) Store cached element references.
+
+
+/*-------------------------------- Functions --------------------------------*/
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+
+//7) Create Reset functionality.
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
